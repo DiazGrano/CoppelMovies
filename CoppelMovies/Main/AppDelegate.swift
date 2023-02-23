1bd1557743a0c7203d4d1a3d2f34d82dd3d0e227
Enrique Grano <ledg1996@gmail.com>
2023-02-22 20:41:35 -0600

    [UPDATE] Estilo de barra de navegación

--- CoppelMovies/Main/AppDelegate.swift
+++ CoppelMovies/Main/AppDelegate.swift
@@ -16,8 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = UINavigationController(rootViewController: CMLoginRouter.create())
+        
+        let navigationController = UINavigationController(rootViewController: CMLoginRouter.create())
+        navigationController.navigationBar.barTintColor = .cmDarkGray
+        navigationController.navigationBar.tintColor = UIColor.cmLightGray
+        navigationController.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.cmLightGray]
+        
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
+        
         return true
     }
 
