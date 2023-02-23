4ff9644b64fbb444209acfdecd83278fda254758
Enrique Grano <ledg1996@gmail.com>
2023-02-22 20:38:07 -0600

    [UPDATE] Extensión UIColor

---
+++ CoppelMovies/Utils/Extensions/UIColor+Extension.swift
@@ -0,0 +1,22 @@
+//
+//  UIColor+Extension.swift
+//  CoppelMovies
+//
+//  Created by Luis Enrique Grano on 22/02/23.
+//
+
+import Foundation
+import UIKit
+
+
+extension UIColor {
+    static var cmDarkGreen = #colorLiteral(red: 0.04705882353, green: 0.08235294118, blue: 0.1019607843, alpha: 1)
+    static var cmDarkGreenCell = #colorLiteral(red: 0.09803921569, green: 0.1529411765, blue: 0.1764705882, alpha: 1)
+    static var cmGreen = #colorLiteral(red: 0.3803921569, green: 0.7921568627, blue: 0.4392156863, alpha: 1)
+    static var cmLighGreen = #colorLiteral(red: 0.3764705882, green: 0.8235294118, blue: 0.4941176471, alpha: 1)
+    static var cmDarkRed = #colorLiteral(red: 0.7843137255, green: 0.3215686275, blue: 0.1568627451, alpha: 1)
+    static var cmLightGray = #colorLiteral(red: 0.7843137255, green: 0.7843137255, blue: 0.7843137255, alpha: 1)
+    static var cmGray = #colorLiteral(red: 0.3882352941, green: 0.3882352941, blue: 0.4, alpha: 1)
+    static var cmDarkGray = #colorLiteral(red: 0.1176470588, green: 0.1176470588, blue: 0.1176470588, alpha: 1)
+    static var cmTransparentDark = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.4)
+}
