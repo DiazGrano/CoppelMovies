83b8adcfee55b28df71ccbd29564b2af3dd71628
Enrique Grano <ledg1996@gmail.com>
2023-02-22 20:37:44 -0600

    [UPDATE] Extensión CGFloat

---
+++ CoppelMovies/Utils/Extensions/CGFloat+Extension.swift
@@ -0,0 +1,22 @@
+//
+//  CGFloat+Extension.swift
+//  CoppelMovies
+//
+//  Created by Luis Enrique Grano on 22/02/23.
+//
+
+import Foundation
+import UIKit
+
+
+extension CGFloat {
+    private static let multiplier = (UIScreen.main.bounds.height > 667 ? 1 : 0.75)
+    
+    static let dimen60 = (60.0 * CGFloat.multiplier)
+    static let dimen40 = (40.0 * CGFloat.multiplier)
+    static let dimen20 = (20.0 * CGFloat.multiplier)
+    static let dimen15 = (15.0 * CGFloat.multiplier)
+    static let dimen10 = (10.0 * CGFloat.multiplier)
+    
+    static let imageSize60 = (60.0 * CGFloat.multiplier)
+}
