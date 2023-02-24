bbbce7a722af906c6536f008fcd175d301d65fdf
Enrique Grano <ledg1996@gmail.com>
2023-02-24 17:03:34 -0600

    [UPDATE] Enum para la llave de los sitios de videos

---
+++ CoppelMovies/Utils/Enums/CMVideoSitesEnum.swift
@@ -0,0 +1,12 @@
+//
+//  CMVideoSitesEnum.swift
+//  CoppelMovies
+//
+//  Created by Luis Enrique Grano on 24/02/23.
+//
+
+import Foundation
+
+enum CMVideoSitesEnum: String {
+    case youtube = "YouTube"
+}
